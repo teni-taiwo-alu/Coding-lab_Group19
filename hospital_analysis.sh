52acfffc52e412e2b88b77955952155e62041358
Moth Manu <sidney.ikenna@gmail.com>
2026-09-23 21:27:48 +0200

    Added water_audit function to calculate average water usage

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -7,4 +7,11 @@ function process_vitals {
 
 process_vitals
 
+function water_audit {
+    avg=$(grep -h "ICU_WATER_RESERVE" active_logs/water_usage_log.log \
+        | awk '{sum += $3; count++} END {if (count > 0) print sum/count; >
+    printf "=== ICU Water Reserve Audit ===\n"
+    printf "Average usage: %.2f litres\n" "$avg"
+}
+water_audit
 

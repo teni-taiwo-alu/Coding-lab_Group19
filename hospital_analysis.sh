b84b103808c8f6c68924556e1bf8570bbe77cabe
Moth Manu <sidney.ikenna@gmail.com>
2026-09-23 21:33:11 +0200

    Added comments for clarity

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Searches log files for CRITICAL and extracts the first second and fourth fields into critical_alerts.txt file
 function process_vitals {
 	grep -h "CRITICAL"  active_logs/heart_rate_log.log active_logs/temperature_log.log active_logs/water_usage_log.log \ 
 	| awk '{print $1, $2, $4}'> reports/critical_alerts.txt
@@ -7,6 +8,7 @@ function process_vitals {
 
 process_vitals
 
+#Searches water usage log for ICU WATER RESERVE and uses awk to calculate the average of the usage values
 function water_audit {
     avg=$(grep -h "ICU_WATER_RESERVE" active_logs/water_usage_log.log \
         | awk '{sum += $3; count++} END {if (count > 0) print sum/count; >
